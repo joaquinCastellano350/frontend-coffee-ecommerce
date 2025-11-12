279b5fb5dc515bc7b87210a2b25ceba6fc6b7a63
joaquinCastellano350 <joaquincastellano350@gmail.com>
2025-11-11 23:21:45 -0300

    header user ui

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,14 @@ import { Component, inject, signal } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from './features/auth/auth.services';
 import { CommonModule, NgIf } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule , NgIf, RouterOutlet, RouterLink ],
+  imports: [CommonModule , NgIf, RouterOutlet, RouterLink, MatDivider, MatMenuModule, MatIconModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -16,7 +20,7 @@ export class App {
     this.auth.refreshSession();
   }
 
-  async onLogout() {
+  async logout() {
     await this.auth.logout();
     this.router.navigate(['']);
   }
